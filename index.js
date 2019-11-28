63e70b311faa8bc1a6c4dcac6524d40563b001e4
Sunim <sunim.54@gmail.com>
2019-11-28 15:10:19 +0545

    Change RNCAppleAuthentication to  AppleSignIn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ export const SignInWithAppleButton = (buttonStyle, callBack) => {
   if(Platform.OS === 'ios'){
     return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
     
-        await RNCAppleAuthentication.requestAsync({
-          scopes: [RNCAppleAuthentication.Scope.FULL_NAME, RNCAppleAuthentication.Scope.EMAIL],
+        await AppleSignIn.requestAsync({
+          scopes: [AppleSignIn.Scope.FULL_NAME, RNCAppleAuthentication.Scope.EMAIL],
         }).then((response) => {
           callBack(response) //Display response
           }, (error) => {
